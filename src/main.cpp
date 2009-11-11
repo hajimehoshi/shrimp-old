1ada04dec9fec188f4bfab8809a5452b9a54f56b
hajimehoshi <hajimehoshi@gmail.com>
2009-11-12 04:05:56 +0900

    optimized main

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,14 @@
-#ifndef __TEST
-
-#include <stdlib.h>
-#include <windows.h>
 #include "Shrimp/Application.h"
-
-int WINAPI WinMain(HINSTANCE hInstance,
-                   HINSTANCE hPrevInstance,
-                   LPSTR lpCmdLine,
-                   int showCmd) {
-  Shrimp::Application& application = Shrimp::Application::GetInstance();
-  return application.Run();;
-}
-
-#else
-
-#include <gtest/gtest.h>
+#ifdef __TEST
+# include <gtest/gtest.h>
+#endif
 
 int main(int argc, char** argv) {
+#ifdef __TEST
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
-}
-
+#else
+  Shrimp::Application& application = Shrimp::Application::GetInstance();
+  return application.Run();
 #endif
+}
