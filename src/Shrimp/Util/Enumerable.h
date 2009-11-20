f5c6b3c99b7ba289b680fbecf37a194c0c302895
hajimehoshi <hajimehoshi@gmail.com>
2009-11-20 15:20:54 +0900

    Fixed a wrong variable's name

--- src/Shrimp/Util/Enumerable.h
+++ src/Shrimp/Util/Enumerable.h
@@ -10,8 +10,8 @@ namespace Shrimp {
       class Enumerable : private Uncopyable {
     public:
       typedef typename T::const_iterator Iterator;
-      Enumerable(T& innnerContainer)
-        : innerContainer(innerContainer) {
+      Enumerable(T& innerContainer_)
+        : innerContainer(innerContainer_) {
       }
       inline Iterator Begin() const {
         return this->innerContainer.begin();
